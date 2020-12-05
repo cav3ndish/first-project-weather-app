ed80efffcc0f577b163f244441d361144f34efc2
cav3ndish <73221879+cav3ndish@users.noreply.github.com>
2020-12-05 18:34:07 +0100

    implement scroll bar fill..part 1

--- src/index.js
+++ src/index.js
@@ -112,4 +112,12 @@ fahrenheit.addEventListener("click", showUnitTempFaren);
 let celsiusTemp = document.querySelector("#celsius");
 celsiusTemp.addEventListener("click", showUnitTempCelsius);
 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.width = scrolled + "15%";
+}
 search("paris");
