2630b5f77c9180bb8661ff46ca8939dcacce477b
cav3ndish <73221879+cav3ndish@users.noreply.github.com>
2020-12-05 18:02:57 +0100

    cleaner code and implementation of temperature change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-let today =  new Date();
-function showDateTime() {
+
+function showDateTime(timestamp) {
+  let today =  new Date(timestamp);
 let days = [
 "Sunday",
 "Monday",
@@ -35,34 +36,36 @@ if (currentTime < 10) {
   if (currentMinutes < 10) {
     currentMinutes = `0${currentMinutes}`;
   }
-let dateTime = document.querySelector("#current-date-time");
-dateTime.innerHTML = ` ${currentDay}, ${date} of ${month}  ${year} , ${currentTime}:${currentMinutes}`;}
-showDateTime(today);
+return ` ${currentDay}    ${month}  ${date}  ${year}  ${currentTime}:${currentMinutes}`;}
+
 
 function showCity(event) {
   event.preventDefault();
-  let input = document.querySelector("#current-city");
-  let output = document.querySelector(".outputCity");
- let city  = ` ${input.value}`;
-  output.innerHTML = city;
- output.innerHTML = output.innerHTML.trim();
- output.innerHTML =  output.innerHTML.charAt(0).toUpperCase() +  output.innerHTML.substring(1).toLowerCase();
+  let cityInput = document.querySelector("#current-city");
+ search(cityInput.value);
+ }
+function infoWeather(response) {
 
-  search(city);
-          }
+let cityElement =  document.querySelector(".city");
+cityElement.innerHTML = response.data.name;          
+cityElement.innerHTML = cityElement.innerHTML.trim();
+cityElement.innerHTML = cityElement.innerHTML.charAt(0).toUpperCase() +  cityElement.innerHTML.substring(1).toLowerCase();
 
-          function infoWeather(response) {
-            document.querySelector(".outputCity").innerHTML = response.data.name;
-             let temperature = Math.round(response.data.main.temp);
- let tempCels = document.querySelector("#temp-change");
-   tempCels.innerHTML = temperature;      
+let tempElement = document.querySelector("#temp");
+   tempElement.innerHTML = Math.round(response.data.main.temp);   
+   celsiusTemperature = response.data.main.temp;
+
 let humid= document.querySelector( "#humidity");
 humid.innerHTML =`Humidity is ${response.data.main.humidity} %`;
+
  let wind=  document.querySelector("#wind");
 wind.innerHTML = `Wind speed is ${Math.round( response.data.wind.speed)} km/h`;
+
 let descript = document.querySelector("#description");
 descript.innerHTML = response.data.weather[0].description;
  descript.innerHTML =  descript.innerHTML.charAt(0).toUpperCase() +  descript.innerHTML.substring(1).toLowerCase();
+ let dateElement = document.querySelector("#current-date-time");
+ dateElement.innerHTML = showDateTime(response.data.dt * 1000);
  let icon = document.querySelector("#icon");
  icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
@@ -76,38 +79,37 @@ function search(city) {
 function currentLocation(position) {
   let apiKey = "0a0b749fb3632bec51c7fbeb7af687a1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(infoWeather);
+   axios.get(apiUrl).then(infoWeather);
 }
 
 function getCurrentCity(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(currentLocation);
-
 }
+
+function showUnitTempFaren(event) {
+ event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+tempElement.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);}
+
+function showUnitTempCelsius(event) {
+  event.preventDefault();
+ let  tempElement = document.querySelector("#temp");
+ tempElement.innerHTML =Math.round( celsiusTemperature);
+  }
+
+let celsiusTemperature = null;
+
 let currentCity = document.querySelector("#locationButton");
 currentCity.addEventListener("click", getCurrentCity);
-
-
 
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", showCity);
 
-search("Rome");
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", showUnitTempFaren);
 
-//function showUnitTempFaren(event) {
- // event.preventDefault();
-  //let celsiusInput = document.querySelector("#temp-change");
-  //let celsius = celsiusInput.innerHTML;
-  //celsius = Number(celsius);
-//celsiusInput.innerHTML = Math.round((celsius * 9) / 5 + 32);}
-//let fahrenheit = document.querySelector("#fahrenheit");
-//fahrenheit.addEventListener("click", showUnitTempFaren);
+let celsiusTemp = document.querySelector("#celsius");
+celsiusTemp.addEventListener("click", showUnitTempCelsius);
 
-//function showUnitTempCelsius(event) {
-  //event.preventDefault();
-  //let farenInput = document.querySelector("#temp-change");
-   //farenInput.innerHTML = `19`;}
-//let celsiusTemp = document.querySelector("#celsius");
-//celsiusTemp.addEventListener("click", showUnitTempCelsius);
-
+search("paris");
